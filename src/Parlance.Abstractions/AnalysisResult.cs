d88af165e6ea4d0ffadc8521021866c8ff7dcb96
Doug Bryant <doug@netinlet.com>
2026-03-28 15:40:23 -0500

    refactor: delete string-based analysis layer (IAnalysisEngine, CSharpAnalysisEngine, CompilationFactory)

--- src/Parlance.Abstractions/AnalysisResult.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Immutable;
-
-namespace Parlance.Abstractions;
-
-public sealed record AnalysisResult(
-    ImmutableList<Diagnostic> Diagnostics,
-    AnalysisSummary Summary);
